1c1f6fe899061c61d7c62a740cfa1883d9885bad
Li Yao <hnkfliyao@gmail.com>
2013-09-25 22:11:57 +0800

    Problem 2 and Problem 3.

--- src/main/scala/Solver.scala
+++ src/main/scala/Solver.scala
@@ -7,4 +7,20 @@ object Solver {
     case _ :: t => last(t)
     case _ => throw new NoSuchElementException
   }
+
+  // Problem 2
+  def penultimate[A](lst: List[A]): A = lst match { 
+    case h :: _ :: Nil => h
+    case _ :: Nil => throw new NoSuchElementException
+    case Nil => throw new NoSuchElementException
+    case _ :: t => penultimate(t)
+  }
+
+  // Problem 3
+  def nth[A](n: Int, lst: List[A]): A = (n, lst) match { 
+    case (_, Nil) => throw new NoSuchElementException
+    case (0, h :: _) => h
+    case (n, _ :: t) if n < 0 => throw new NoSuchElementException
+    case (_, _ :: t) => nth(n - 1, t)
+  }
 }
--- src/test/scala/Tester.scala
+++ src/test/scala/Tester.scala
@@ -3,11 +3,40 @@ import Solver.Solver._
 
 class SolverTest extends Suite { 
   // Problem 1
-  def testLast() { 
+  def testLast { 
     assert(last(List(1)) === 1)
     assert(last(List(1, 1, 2, 3, 5, 8)) === 8)
     intercept[NoSuchElementException] { 
       last(Nil)
+    }
+  }
+
+  // Problem 2
+  def testPenultimate = {
+    assert(penultimate(List(1, 2)) === 1)
+    assert(penultimate(List(1, 1, 2, 3, 5, 8)) === 5)
+    intercept[NoSuchElementException] { 
+      penultimate(List(1))
+    }
+    intercept[NoSuchElementException] { 
+      penultimate(Nil)
+    }
+  }
+
+  // Problem 3
+  def testNth = {
+    val lst = List(1, 2, 3)
+    assert(nth(0, lst) === 1)
+    assert(nth(1, lst) === 2)
+    assert(nth(2, lst) === 3)
+    intercept[NoSuchElementException] { 
+      nth(-1, lst)
+    }
+    intercept[NoSuchElementException] { 
+      nth(0, Nil)
+    }
+    intercept[NoSuchElementException] { 
+      nth(1, List(1))
     }
   }
 }
