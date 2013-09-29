99cfeecb95676ba66e7876f8c3afd2b5f337c700
Yao Li <hnkfliyao@gmail.com>
2013-09-29 13:35:40 +0800

    Problem 33.

--- src/main/scala/S99Arithmetic.scala
+++ src/main/scala/S99Arithmetic.scala
@@ -1,6 +1,14 @@
 package S99
 
+class S99Int(value: Int) {
+  import S99Arithmetic._
+  
+  def isCoprimeTo(n: Int): Boolean = gcd(value, n) == 1
+}
+
 object S99Arithmetic { 
+  implicit def intToS99Int(x: Int) = new S99Int(x)
+
   def gcd(a: Int, b: Int): Int = { 
     if (b == 0) a
     else { 
--- src/test/scala/S99ArithemeticTester.scala
+++ src/test/scala/S99ArithemeticTester.scala
@@ -1,10 +1,16 @@
 import org.scalatest.Suite
 import S99.S99Arithmetic._
+import S99.S99Int
 
 class ArithmeticTest extends Suite { 
   // Problem 32
   def testGcd = { 
     assert(gcd(36, 63) === 9)
     assert(gcd(35, 64) === 1)
+  }
+
+  def testIsCoprimeTo = { 
+    assert(35.isCoprimeTo(64) === true)
+    assert(36.isCoprimeTo(64) === false)
   }
 }
