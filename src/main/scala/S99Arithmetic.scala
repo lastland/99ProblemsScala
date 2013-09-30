8dcf1aaad96998a4cb3c342c880b2dcaf1ea222e
Yao Li <hnkfliyao@gmail.com>
2013-09-30 12:27:31 +0800

    Problem 31.

--- src/main/scala/S99Arithmetic.scala
+++ src/main/scala/S99Arithmetic.scala
@@ -1,7 +1,7 @@
 package S99
 import scala.math._
+import scala.collection.immutable.Stream
 import S99List._
-
 
 object S99Arithmetic { 
   implicit def intToS99Int(x: Int) = new S99Int(x)
@@ -13,8 +13,14 @@ object S99Arithmetic {
       gcd(b, a % b)
     }
   }
+
+  val primes = 2 #:: Stream.from(3, 2).filter(_ isPrime)
   
   case class S99Int(value: Int) {
+    // Problem 31
+    def isPrime: Boolean =
+      (value > 1) && (primes.takeWhile(_ <= sqrt(value).toInt).forall(value % _ != 0))
+
     // Problem 33
     def isCoprimeTo(n: Int): Boolean = gcd(value, n) == 1
 
--- src/test/scala/S99ArithemeticTester.scala
+++ src/test/scala/S99ArithemeticTester.scala
@@ -2,6 +2,18 @@ import org.scalatest.Suite
 import S99.S99Arithmetic._
 
 class ArithmeticTest extends Suite { 
+  // Problem 31
+  def testIsPrime = { 
+    assert(1.isPrime === false)
+    assert(2.isPrime === true)
+    assert(3.isPrime === true)
+    assert(9.isPrime === false)
+    assert(996.isPrime === false)
+    assert(997.isPrime === true)
+    assert(3569.isPrime === false)
+    assert(3571.isPrime === true)
+  }
+  
   // Problem 32
   def testGcd = { 
     assert(gcd(36, 63) === 9)
